460dfed00a6a6f0f4d953e7a24bd92507170e2a8
Woodland4678 <robotics@woodland.on.ca>
2020-02-11 16:55:18 -0500

    Colour Sensor

--- src/main/include/Subsystems/Drivetrain.h
+++ src/main/include/Subsystems/Drivetrain.h
@@ -68,6 +68,7 @@ Drivetrain();
 	bool ml_ValidTarget = false;
 	double ml_targetHorizontial;
 	double ml_targetVertical;
+	double ml_TargetDistance;
 
 	void setLimeLED(bool state);
 	void SetLimeFar();
