2895d6a81abd43bc2b2f9e3821e9b46806bb0c36
Woodland4678 <robotics@woodland.on.ca>
2020-03-08 14:55:33 -0400

    auto

--- src/main/cpp/Commands/Autonomous/AutoTenBall.cpp
+++ src/main/cpp/Commands/Autonomous/AutoTenBall.cpp
@@ -23,17 +23,17 @@ AutoTenBall::AutoTenBall(): frc::Command() {
 
 // Called just before this Command runs the first time
 void AutoTenBall::Initialize() {
-    path1 = new PathFinder(0.02,2,2,1,0.7112);  // cycle time (s), max velocity (m/s), max acceleration (m/s^2), max jerk (m/s^3), distance between wheels (m)
+    path1 = new PathFinder(0.02,3,2,1,0.7112);  // cycle time (s), max velocity (m/s), max acceleration (m/s^2), max jerk (m/s^3), distance between wheels (m)
     path1->createNewPath();
     path1->addWayPoint(0, 0, 0);  // X is in front of robot, -X is behind, -Y is left, +Y is right
-    //path1->addWayPoint(0.75, 0, 0); //2.44, 0, 0 - meters
-    path1->addWayPoint(2.35, 0, 45); //2.44, 0, 0 - meters
+    path1->addWayPoint(1.85, 0, 0); //2.44, 0, 0 - meters
+    path1->addWayPoint(2.42, 0.32, 40.5); //2.44, 0, 0 - meters
     path1->makePath();
 
     path2 = new PathFinder(0.02,2,2,1,0.7112);
     path2->createNewPath();
-    path2->addWayPoint(0, 0, 0);
-    path2->addWayPoint(-2.35,-0.5,0);
+    path2->addWayPoint(2.42, 0.32, 40.5);
+    path2->addWayPoint(0,0,0);
     path2->makePath();
 
     path3 = new PathFinder(0.02,3,2,1,0.7112);
@@ -66,7 +66,7 @@ void AutoTenBall::Execute() {
                 autoStep = delay;
                 nextAutoStep = returnToShootFirstVolley;
                 delayCount = 10;
-                path2->startTraverse(frc::Timer::GetFPGATimestamp());
+                path1->startTraverse(frc::Timer::GetFPGATimestamp());
                 rVel = 0;
                 lVel = 0;
                 cnt = 0;
@@ -78,6 +78,7 @@ void AutoTenBall::Execute() {
         break;
         case returnToShootFirstVolley:
             if (path2->traverse(frc::Timer::GetFPGATimestamp(),&rVel,&lVel,Robot::drivetrain->getGyroReading())) {   // cnt = how far down the path are you, right velocity (m/s), left velocity (m/s)
+            //if (path1->inverse_traverse(frc::Timer::GetFPGATimestamp(),&rVel,&lVel,Robot::drivetrain->getGyroReading())) {   // cnt = how far down the path are you, right velocity (m/s), left velocity (m/s)
                 //autoStep = delay;
                 //delayCount = 75;
                 //nextAutoStep = runTrench;
